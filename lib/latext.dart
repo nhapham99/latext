f0b7fad5dc179ab2e09cb2dec793789cef960243
nhapham <nha.pham@ikemso.com>
2024-11-13 11:12:08 +0700

    v1.1.3

--- lib/latext.dart
+++ lib/latext.dart
@@ -165,6 +165,10 @@ class LaTexTState extends State<LaTexT> {
       return fontSize / 8;
     }
 
+    if (RegExp(r'^-?\d+(\,\d+)?').hasMatch(text)) {
+      return fontSize / 8;
+    }
+
     if (RegExp(r'^-?\d+(\.\d+)?').hasMatch(text)) {
       return fontSize / 16;
     }
